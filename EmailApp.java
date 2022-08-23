4a3444be2a5e0cfd42a2a35cbaaf1b98c4469fc6
sweetpanqueca <53286067+sweetpanqueca@users.noreply.github.com>
2022-08-23 14:34:00 -0300

    Add files via upload

---
+++ EmailApp.java
@@ -0,0 +1,10 @@
+package emailAdministrationApplication;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		Email em1 = new Email("Guilherme", "Alves");
+		System.out.println(em1.showInfo());
+	}
+
+}
